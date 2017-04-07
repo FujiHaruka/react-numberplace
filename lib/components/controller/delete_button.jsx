1273c782d8ed40d4edec26b55078951dcc5e7f47
FujiHaruka <fujiharuka@realglobe.jp>
2017-04-08 01:17:33 +0900

    Impl delete button

--- lib/components/controller/delete_button.jsx
+++ lib/components/controller/delete_button.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import c from 'classnames'
 import styles from '../../styles/buttons.css'
+import { getState, setState }  from '../../state_exposer'
+import _ from 'lodash'
 
 const DeleteButton = () =>
-  <div className={c(styles.action, styles.nomalColor)}>
+  <div
+    className={c(styles.action, styles.nomalColor)}
+    onClick={() => {
+      let {
+        cellState,
+        focusedCell
+      } = getState()
+      if (_.isEmpty(focusedCell)) {
+        return
+      }
+      setState({
+        cellState: cellState.update({
+          value: 0,
+          ...focusedCell
+        })
+      })
+    }}
+    >
     Delete
   </div>
 
